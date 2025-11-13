d645fe03a4de0eef73b880bc723d4cd6990add77
Kaavya Barathi <2512+Kaavya2512@users.noreply.github.com>
2025-11-13 18:19:37 +0530

    foodcategorylist added

---
+++ src/app/foodcategorylist/foodcategorylist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foodcategorylist',
+  standalone: true,
+  imports: [],
+  templateUrl: './foodcategorylist.component.html',
+  styleUrl: './foodcategorylist.component.css'
+})
+export class FoodcategorylistComponent {
+
+}

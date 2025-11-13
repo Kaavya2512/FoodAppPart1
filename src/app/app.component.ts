09fc4dcc832fc803c108c658143b57548b7d6033
Kaavya Barathi <2512+Kaavya2512@users.noreply.github.com>
2025-11-13 10:26:49 +0530

    few errors corrected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {basicfooddatacomponent} from './basicfooddata/basicfooddata.component';
+import {fooditemsummarycomponent} from './fooditemsummary/fooditemsummary.component';
 
 @Component({
   selector: 'app-root',
--- src/app/fooditemsummary/fooditemsummary.component.ts
+++ src/app/fooditemsummary/fooditemsummary.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-fooditemsummary',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './fooditemsummary.component.html',
   styleUrl: './fooditemsummary.component.css'
 })
